810f62366ef2383636b16d1f3494622eb8d88cec
lh2144 <hlian@stevens.edu>
2020-10-06 23:01:44 -0500

    initial commit for back end

--- back-end/app.ts
+++ back-end/app.ts
@@ -1,34 +1,29 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import express, { request, response } from 'express';
+import path from 'path';
+import bodyParser from 'body-parser';
+import usersRouter from './routes/users';
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
+const app = express();
+const port = 8080;
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/', (req , res ) => {
+    res.send('Hello world');
+});
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use((req , res , next) => {
+  next();
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err: any, req: any, res: any, next: any) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -38,4 +33,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+app.listen(port, () => console.log(port));
+
+export default app;
