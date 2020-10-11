43a80cd6c42c96452577f94dd704849211aead70
lh2144 <hlian@stevens.edu>
2020-10-10 21:20:37 -0500

    add interceptor

---
+++ front-end/src/app/shared/service/interceptor/http.intercepot.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpEvent, HttpHandler, HttpRequest, HttpErrorResponse } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+@Injectable()
+export class AuthAndErrorInterceptor implements HttpInterceptor {
+  public intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const sessionToken = localStorage.getItem('sessionToken');
+    if (sessionToken) {
+      req = req.clone({
+        setHeaders: {
+          authorization: `Bearer ${sessionToken}`
+        }
+      });
+    }
+    return next.handle(req).pipe(
+        catchError((error: any) => {
+            if (error instanceof HttpErrorResponse) {
+                console.log(error);
+            }
+            return of(error);
+        })
+    );
+  }
+}
