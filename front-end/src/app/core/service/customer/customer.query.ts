49d7134da85754a177cd38d2c44305e06d36b904
lh2144 <hlian@stevens.edu>
2020-10-10 19:01:12 -0500

    update user query

--- front-end/src/app/core/service/customer/customer.query.ts
+++ front-end/src/app/core/service/customer/customer.query.ts
@@ -3,10 +3,12 @@ import { Query } from '@datorama/akita';
 import { UserStore } from './customer.store';
 import { UserState } from './user.model';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class UserQuery extends Query<UserState> {
-  public constructor(protected store: UserStore) {
-    super(store);
-  }
+    public allState$ = this.select();
+    public currentUser = this.select('name');
 
+    public constructor(protected store: UserStore) {
+        super(store);
+    }
 }
