917ac58c33483816954828c930f5277ea9acbded
lh2144 <hlian@stevens.edu>
2020-10-13 22:46:52 -0500

    set up homepage products feature

--- front-end/src/app/core/service/product/product.service.ts
+++ front-end/src/app/core/service/product/product.service.ts
@@ -1,6 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { ProductState, ProductStore } from './product.store';
 
 @Injectable({ providedIn: 'root' })
 export class ProductService {
-    public constructor() {}
+    public constructor(
+        public http: HttpClient,
+        public productStore: ProductStore
+    ) {}
+
+    public getAllProduct(): Observable<ProductState[]> {
+      return this.http.get(environment.base + 'products').pipe(map((res: []) => {
+          this.productStore.set(res);
+          return res;
+      }));
+    }
 }
