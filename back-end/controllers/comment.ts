2afd079577385346a7a6e05af8a5baf6e3881996
lh2144 <hlian@stevens.edu>
2020-10-16 23:20:02 -0500

    add getcomment by id

--- back-end/controllers/comment.ts
+++ back-end/controllers/comment.ts
@@ -1,7 +1,11 @@
 import Comment from '../models/comment';
-import { Request, Response } from 'express';
+import { Request, response, Response } from 'express';
 import mongoose from 'mongoose';
-const ObjectId = mongoose.Schema.Types.ObjectId;
+import Product from '../models/product';
+import comment from '../models/comment';
+
+const ObjectId = mongoose.Types.ObjectId;
+
 const createComment = async (req: Request, res: Response, next) => {
     const auth = req.get('Authorization');
     if (!auth) {
@@ -9,16 +13,56 @@ const createComment = async (req: Request, res: Response, next) => {
         error['statusCode'] = 422;
     }
     const { title, nickName, detail, productId, rating } = req.body;
+    console.log(new ObjectId(productId));
     const comment = new Comment({ title, nickName, detail, rating, productId: new ObjectId(productId) });
     try {
-        const res = await comment.save();
+        const response = await comment.save();
+        const product = await Product.findById(productId);
+        if (product) {
+            if (product['reviews'].length < 10) {
+                product['reviews'].push(response);
+            } else if (product['reviews'].length >= 10) {
+                product['reviews'].shift();
+                product['reviews'].push(response);
+            }
+        }
+
+        res.status(200).json({
+            title: response['title'],
+            nickName: response['nickName'],
+            detail: response['detail'],
+            rating: response['rating'],
+            productId: response['productId'].toString(),
+        });
     } catch (err) {
         if (!err['statusCode']) {
             err['statusCode'] = 500;
             next(err);
         }
     }
-    res.status(200).json({ ...res });
 };
-
-export { createComment };
+const getCommentsById = async (req: Request, res: Response, next) => {
+    const productId = req.params.id;
+    if (!productId) {
+        const error = new Error('bad request, url should come with productId params');
+        error['statusCode'] = 400;
+        throw error;
+    }
+    try {
+        const convertId = new ObjectId(productId);
+        const comments = await Comment.find({ productId: convertId });
+        const count = comments.length;
+        res.status(200).json({
+            data: {
+                comments: [...comments],
+                total: count
+            }
+        });
+    } catch (err) {
+        if (!err['statusCode']) {
+            err['statusCode'] = 500;
+            next(err);
+        }
+    }
+};
+export { createComment, getCommentsById };
--- back-end/routes/comment.ts
+++ back-end/routes/comment.ts
@@ -1,7 +1,8 @@
 import express from 'express';
-import { createComment } from '../controllers/comment';
+import { createComment, getCommentsById } from '../controllers/comment';
 const router = express.Router();
 
 router.post('/createComment', createComment);
+router.get('/comment/:id', getCommentsById);
 
 export default router;
