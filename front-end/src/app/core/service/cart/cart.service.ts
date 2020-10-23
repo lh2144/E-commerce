3cf390abe02151b2715ca286f8ed79b12e4d8954
lh2144 <hlian@stevens.edu>
2020-10-23 18:56:18 -0500

    update cart service

--- front-end/src/app/core/service/cart/cart.service.ts
+++ front-end/src/app/core/service/cart/cart.service.ts
@@ -1,7 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { Cart } from './cart.model';
 import { CartStore } from './cart.store';
 
 @Injectable({ providedIn: 'root' })
 export class CartService {
-    public constructor(public cartStore: CartStore) {}
+    public constructor(public cartStore: CartStore, public http: HttpClient) {}
+
+    public getCart(): Observable<Cart> {
+      return this.http.get(environment.base + 'buyflow/cart').pipe(map((res) => {
+        this.cartStore.update(res);
+        return (res as Cart);
+      }));
+    }
+
+    public postCart(payload: any): Observable<Cart> {
+      return this.http.post(environment.base + 'buyflow/cart', payload).pipe(map(res => {
+        this.cartStore.update(res);
+        return (res as Cart);
+      }));
+    }
 }
