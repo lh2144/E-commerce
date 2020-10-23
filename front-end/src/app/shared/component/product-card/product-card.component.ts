a8151900ae2b7458cb866ff6c2cba24810369358
lh2144 <hlian@stevens.edu>
2020-10-22 21:24:08 -0500

    add style input for product-card and icon in index

--- front-end/src/app/shared/component/product-card/product-card.component.ts
+++ front-end/src/app/shared/component/product-card/product-card.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { ProductQuery } from 'service';
+import { Product, ProductQuery } from 'service';
 import { ProductState } from 'service';
 
 @Component({
@@ -8,7 +8,8 @@ import { ProductState } from 'service';
   styleUrls: ['./product-card.component.scss']
 })
 export class ProductCardComponent implements OnInit {
-  @Input() public product: ProductState;
+  @Input() public product: Product;
+  @Input() public layout: string;
   public constructor(public productQuery: ProductQuery ) { }
 
   public ngOnInit(): void {
