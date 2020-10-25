37f0a56f7428be5545786ae00261ef6ef121332b
lh2144 <hlian@stevens.edu>
2020-10-25 16:08:41 -0500

    setup address component

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { CartComponent } from './pages/cart/cart.component';
 import { HomeComponent } from './pages/home/home.component';
 import { PostCommentComponent } from './pages/post-comment/post-comment.component';
 import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
+import { AddressComponent } from './shared/component/address/address.component';
 
 const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -14,7 +15,8 @@ const routes: Routes = [
     { path: 'register', component: RegisterComponent },
     { path: ':id/product-detail', component: ProductDetailComponent },
     { path: 'postComment/:id', component: PostCommentComponent },
-    { path: 'mybasket', component: CartComponent}
+    { path: 'mybasket', component: CartComponent},
+    { path: 'address', component: AddressComponent }
 ];
 
 @NgModule({
---
+++ front-end/src/app/shared/component/address/address.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'my-address',
+  templateUrl: './address.component.html',
+  styleUrls: ['./address.component.scss']
+})
+export class AddressComponent implements OnInit {
+
+  public constructor() { }
+
+  public ngOnInit(): void {
+  }
+
+}
